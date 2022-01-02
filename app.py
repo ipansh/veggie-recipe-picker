4175d685950350dc261573a9f338d36593474f25
Ilya <ilya.panshenskov@italki.com>
2022-01-02 12:30:36 +0800

    connecting to db

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def home():
